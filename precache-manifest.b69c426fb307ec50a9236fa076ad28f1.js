cd0f11024b8f21136b05422060ad76ed9a5fadff
Jin Wang <wangatqueens@gmail.com>
2019-08-22 23:54:29 -0400

    Updates

---
+++ precache-manifest.b69c426fb307ec50a9236fa076ad28f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f043c48c841eaac1617d21c2aec6ee3",
+    "url": "/favourite-recipe/index.html"
+  },
+  {
+    "revision": "cd945bea6e5d26920b5e",
+    "url": "/favourite-recipe/static/css/main.8ccaf271.chunk.css"
+  },
+  {
+    "revision": "b4a032c481a264470fb5",
+    "url": "/favourite-recipe/static/js/2.9e1ad105.chunk.js"
+  },
+  {
+    "revision": "cd945bea6e5d26920b5e",
+    "url": "/favourite-recipe/static/js/main.f481c407.chunk.js"
+  },
+  {
+    "revision": "9f397c22e93b064c6d03",
+    "url": "/favourite-recipe/static/js/runtime~main.7e39bc00.js"
+  },
+  {
+    "revision": "bea35746fb7cdd67b15ecf0622d8cc85",
+    "url": "/favourite-recipe/static/media/Kalam-Regular.bea35746.ttf"
+  },
+  {
+    "revision": "eeca044ef8fa4525d3923045f1adbd6c",
+    "url": "/favourite-recipe/static/media/ZhiMangXing-Regular.eeca044e.ttf"
+  }
+]);
